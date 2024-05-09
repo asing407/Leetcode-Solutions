c91af9e45fe6b26bc010978183bd05821ec19070
Anshumaan Singh <144586980+asing407@users.noreply.github.com>
2024-05-09 17:41:36 +0530

    Time: 580 ms (68.01%), Space: 65.6 MB (16.8%) - LeetHub

---
+++ 2881-create-a-new-column/2881-create-a-new-column.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def createBonusColumn(employees: pd.DataFrame) -> pd.DataFrame:
+    employees['bonus'] = 2*employees['salary']
+
+    return employees
+
+    print(employees)
