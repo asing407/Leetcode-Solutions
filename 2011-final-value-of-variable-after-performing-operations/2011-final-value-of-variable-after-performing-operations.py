1ab43677a897e84402e8e2f3d217517a20b309ee
Anshumaan Singh <144586980+asing407@users.noreply.github.com>
2024-05-10 17:31:07 +0530

    Time: 56 ms (39.3%), Space: 16.5 MB (94.73%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.py
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.py
@@ -1,16 +1,10 @@
 class Solution:
     def finalValueAfterOperations(self, operations: List[str]) -> int:
-        
         currx = 0
+
         for i in operations:
-            
-            if i == "X--" or i == "--X":
+            if i == '--X' or i == 'X--':
                 currx -= 1
             else:
                 currx += 1
         return currx
-                
-            
-        
-        
-        
