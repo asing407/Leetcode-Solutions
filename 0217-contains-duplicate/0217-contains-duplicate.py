2910b65116fcbdc1e919b6699502b8a7ac4ce11d
Anshumaan Singh <144586980+asing407@users.noreply.github.com>
2024-10-21 03:50:58 +0530

    Time: 189 ms (97.36%), Space: 17.3 MB (100%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,17 +1,11 @@
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        ht = {}
-
-        for i in range(len(nums)):
-            if nums[i] not in ht:
-                ht[nums[i]] = 1
-            else:
-                ht[nums[i]] += 1
-                
-
-        for i in range(len(nums)):
-            if ht[nums[i]] >= 2:
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        nums = sorted(nums)
+        for i in range(1,len(nums)):
+            if nums[i] == nums[i-1]:
                 return True
-
-        return False
-                
+                return False
