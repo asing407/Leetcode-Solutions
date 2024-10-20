2460187c39ca534bb8b103e24435f65e61e27635
Anshumaan Singh <144586980+asing407@users.noreply.github.com>
2024-10-21 04:37:38 +0530

    Time: 736 ms (5.2%), Space: 23.8 MB (99.26%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
@@ -2,6 +2,6 @@ class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
         nums.sort()
         
-        dict = collections.Counter(nums)
+        d = collections.Counter(nums)
         
-        return[x for x in range(1,len(nums) + 1)if x not in dict]
+        return [x for x in range(1, len(nums) + 1) if x not in d]
