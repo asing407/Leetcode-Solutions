ff7b9c65a548d6386d34b6a9ece05a9afd7bffb1
Anshumaan Singh <144586980+asing407@users.noreply.github.com>
2024-10-21 18:58:05 +0530

    Time: 0 ms (100%), Space: 16.8 MB (6.21%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,12 +1,26 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n == 0 or n == 1:
-            return 1
-        a = 1
-        b = 1
+        # if n == 0 or n == 1:
+        #     return 1
+        
+        # a = 1 
+        # b = 1
 
-        for i in range(2, n + 1):
-            c = a + b
-            a = b
-            b = c
-        return b
+        # for i in range(2, n + 1):
+        #     c = a + b
+        #     a = b j
+        #     b = c
+        # return b
+
+        
+
+        memo = {0: 1, 1: 1}
+
+        def climb(n):
+            # Check if the result is already computed
+            if n not in memo:
+                # Recursive call with memoization
+                memo[n] = climb(n - 1) + climb(n - 2)
+            return memo[n]
+
+        return climb(n)
