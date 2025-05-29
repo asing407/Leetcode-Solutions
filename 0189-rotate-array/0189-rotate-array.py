fd124ec9e06b729b7ddfced2fa06f4f1ab6af2b5
Anshumaan Singh <144586980+asing407@users.noreply.github.com>
2025-05-29 21:21:37 +0100

    Time: 7 ms (46.53%), Space: 25.7 MB (47.39%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,17 +1,18 @@
-        
 class Solution:
-    def reversedarray(self, left, right, arr):
-        
+    def reversedarr(self, left, right, arr):
         while left < right:
-                arr[left], arr[right] = arr[right], arr[left]
-                left += 1
-                right -= 1
-                
+            arr[left], arr[right] = arr[right], arr[left]
+            left += 1
+            right -= 1
     def rotate(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        k %= len(nums)          
-        self.reversedarray(0, len(nums)-1, nums)
-        self.reversedarray(0, k-1, nums)
-        self.reversedarray(k, len(nums) - 1, nums)
+        #nums = [1,2,3,4,5,6,7]
+        k %= len(nums)
+        self.reversedarr(0,len(nums)- 1, nums)
+        self.reversedarr(0, k- 1, nums)
+        self.reversedarr(k, len(nums) - 1, nums)
+        
+
+
