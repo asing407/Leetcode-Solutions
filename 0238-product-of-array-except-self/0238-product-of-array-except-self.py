3c99bf6a9c078fe19ab504bf10d0d37bfd08cf4e
Anshumaan Singh <144586980+asing407@users.noreply.github.com>
2026-01-03 17:44:48 +0000

    Time: 23 ms (63.47%), Space: 23.1 MB (96.12%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -6,14 +6,19 @@ class Solution:
 
         for i in range(len(nums)):
             res[i] *= pref
+
             pref *= nums[i]
 
+        
         postfix = 1
 
-        for i in range(len(nums) -1,-1, -1):
+        for i in range(len(nums)-1,-1,-1):
             res[i] *= postfix
             postfix *= nums[i]
 
         return res
+
+            
+
 
 
